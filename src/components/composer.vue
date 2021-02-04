0e7e1494fbfeb4d6db4f2711b20edb48b91e7332
Ariadne Conill <ariadne@dereferenced.org>
2021-02-03 20:26:50 -0700

    begin work on composer

---
+++ src/components/composer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="composer">
+    <div class="composer-me">
+      <img :src="$root.actor.iconURL()" alt="$root.actor.data.name">
+    </div>
+
+    <div class="composer-container">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Composer',
+}
+</script>
+
+<style>
+.composer {
+  display: flex;
+  margin-bottom: 1em;
+}
+
+.composer-me {
+  max-width: 80px;
+  flex: 1;
+}
+
+.composer-me img {
+  max-width: 64px;
+  max-height: 64px;
+  border-radius: 4px;
+}
+
+.composer-container {
+  background: #eeeeee;
+  border-radius: 4px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="dashboard">
+    <Composer />
     <InboxCollection :actor="$root.actor" />
   </div>
 </template>
 
 <script>
 import InboxCollection from '@/components/inbox-collection'
+import Composer from '@/components/composer'
 
 export default {
   name: 'Home',
-  components: { InboxCollection },
+  components: { InboxCollection, Composer },
   mounted() {
     if (!this.$root.actor)
       this.$router.push('/.well-known/jejune/login')
