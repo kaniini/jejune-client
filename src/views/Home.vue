b63a63cf59dc2d755bfbc10ceffd7de5fee45350
Ariadne Conill <ariadne@dereferenced.org>
2021-02-13 01:16:34 -0700

    home: do some layout work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="dashboard">
-    <Composer />
-    <InboxCollection :actor="$root.actor" />
+  <div class="dashboard-parent">
+    <div class="dashboard">
+      <div class="dashboard-stream">
+        <Composer />
+        <InboxCollection :actor="$root.actor" />
+      </div>
+
+      <div class="dashboard-sidebar">
+        <div class="dashboard-widget">
+          <h4>Notifications</h4>
+          <p>Not actually implemented yet!</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,9 +31,32 @@ export default {
 </script>
 
 <style>
-.dashboard {
+.dashboard-parent {
    padding: 1em;
    max-width: 1200px;
    margin: auto;
+}
+
+.dashboard {
+   display: flex;
+}
+
+.dashboard-stream {
+   flex: 2;
+   max-width: 800px;
+}
+
+.dashboard-sidebar {
+   flex: 1;
+   max-width: 400px;
+   margin-left: 1em;
+   margin-right: 1em;
+}
+
+.dashboard-widget h4 {
+   margin-top: 0;
+   color: #666688;
+   border-bottom: 1px solid #666666;
+   text-transform: uppercase;
 }
 </style>
